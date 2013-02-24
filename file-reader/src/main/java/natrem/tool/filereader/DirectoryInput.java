22875454ef81ca6a4a8b1cf87e5c018ad07b565b
Pascal Verdage <pascal.verdage@gmail.com>
2013-02-24 22:35:31 +0100

    refactor using guava utilities

--- file-reader/src/main/java/natrem/tool/filereader/DirectoryInput.java
+++ file-reader/src/main/java/natrem/tool/filereader/DirectoryInput.java
@@ -2,15 +2,16 @@ package natrem.tool.filereader;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.Iterator;
-import java.util.NoSuchElementException;
+import java.util.List;
 import java.util.Objects;
 
 import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.LineIterator;
 import org.apache.commons.io.filefilter.CanReadFileFilter;
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
+
+import com.google.common.collect.Iterators;
 
 /**
  * {@link FileContentInput} to iterate through all lines of all readable files
@@ -21,10 +22,8 @@ import org.apache.commons.logging.LogFactory;
  */
 public class DirectoryInput implements FileContentInput {
 
-    private static final Log log = LogFactory.getLog(DirectoryInput.class);
-
     private File directory;
-    private DirectoryLineIterator iterator;
+    private List<LineIterator> lineIterators;
 
     public DirectoryInput(File directory) {
         Objects.requireNonNull(directory);
@@ -38,113 +37,29 @@ public class DirectoryInput implements FileContentInput {
 
     @Override
     public Iterator<String> iterator() {
-        if (iterator != null) {
+        if (lineIterators != null) {
             throw new IllegalStateException("there is already an iterator. Close it first");
         }
-        // checkInternalState();
-        iterator = new DirectoryLineIterator();
-        return iterator;
-    }
-
-    private class DirectoryLineIterator implements Iterator<String> {
-
-        private Iterator<File> fileIterator;
-        private FileInput currentFileContentInput;
-        // should never be null
-        private Iterator<String> currentFileIterator = Collections.emptyIterator();
-
-        public DirectoryLineIterator() {
-            if (directory.canRead() && directory.isDirectory()) {
-                fileIterator = FileUtils.iterateFiles(directory,
-                        CanReadFileFilter.CAN_READ, null);
-            } else {
-                fileIterator = Collections.emptyIterator();
-            }
-        }
-
-        @Override
-        public boolean hasNext() {
-            // checkInternalState is copied from LineIterator but is depending
-            // on the contract
-            // of the close method. add it when the contact is defined and with
-            // tests !
-            // checkInternalState();
-
-            // if the current File Iterator is empty
-            // then switch to next file
-            while (!currentFileIterator.hasNext() && fileIterator.hasNext()) {
-                closeCurrentFileContentInput();
-                File newFile = fileIterator.next();
-                setFileIterator(newFile);
-            }
-
-            return currentFileIterator.hasNext();
-        }
-
-        /**
-         * @param newFile
-         *            new File to use to configure the currentFileContentInput
-         *            and cuurentFileIterator
-         */
-        private void setFileIterator(File newFile) {
-            currentFileContentInput = new FileInput(newFile);
-            try {
-                currentFileIterator = currentFileContentInput.iterator();
-            } catch (IllegalStateException e) {
-                currentFileIterator = Collections.emptyIterator();
-            }
-        }
-
-        /**
-         * Close the current FileContentInput
-         */
-        private void closeCurrentFileContentInput() {
-            try {
-                if (currentFileContentInput != null) {
-                    currentFileContentInput.close();
-                }
-            } catch (IOException e) {
-                log.warn("Cannot close current file: " + getCurrentFileName(), e);
-            }
-        }
-
-        private String getCurrentFileName() {
-            if (currentFileContentInput != null) {
-                return currentFileContentInput.getFile().getName();
-            }
-            return "";
-        }
-
-        @Override
-        public String next() {
-            // checkInternalState();
-            if (!hasNext()) {
-                throw new NoSuchElementException("No more lines");
-            }
-            return currentFileIterator.next();
-        }
-
-        @Override
-        public void remove() {
-            // checkInternalState();
-            currentFileIterator.remove();
+        
+        lineIterators = new ArrayList<LineIterator>();
+        
+        for (Iterator<File> fileIterator = FileUtils.iterateFiles(directory, CanReadFileFilter.CAN_READ, null); fileIterator.hasNext();) {
+            File f = fileIterator.next();
+            lineIterators.add(new FileInput(f).iterator());
         }
         
-        public void close() throws IOException {
-            fileIterator = Collections.emptyIterator();
-            if (currentFileContentInput != null) {
-                currentFileContentInput.close();
-                currentFileContentInput = null;
-            }
-            currentFileIterator = Collections.emptyIterator();
-        }
+        Iterator<String> iterator = Iterators.concat(lineIterators.iterator());
+        
+        return iterator;
     }
 
     @Override
     public void close() throws IOException {
-        if (iterator != null) {
-            iterator.close();
-            iterator = null;
+        if (lineIterators != null) {
+            for (LineIterator lineIterator : lineIterators) {
+                lineIterator.close();
+            }
+            lineIterators = null;
         }
     }
 }
